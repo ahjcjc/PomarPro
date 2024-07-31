1bb28392fd95bd8c7d62509eabe602c593335ccc
unknown <joao.cuqui@estudante.sesisenai.org.br>
2024-07-31 13:25:41 -0300

    pomarpro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './pages/login/login.component';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { REACTIVE_NODE } from '@angular/core/primitives/signals';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CardUsuarioComponent } from './pages/usuario/card-usuario/card-usuario.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    MenuComponent,
+    CardUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [provideHttpClient(), provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/pages/usuario/card-usuario/card-usuario.component.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService } from '../../../services/usuario.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-card-usuario',
+  templateUrl: './card-usuario.component.html',
+  styleUrl: './card-usuario.component.scss'
+})
+export class CardUsuarioComponent {
+
+  constructor(
+    private usuarioService:UsuarioService,
+    private snackbar:MatSnackBar
+){
+
+}
+
+//inicialize o formulario
+formulario:FormGroup = new FormGroup({
+  id:new FormControl(null),
+  nome:new FormControl('',Validators.required),
+  sobrenome:new FormControl('',Validators.required),
+  endereco:new FormControl('',Validators.required),
+  telefone:new FormControl('',Validators.required),
+  email:new FormControl('',Validators.required),
+  login:new FormControl('',Validators.required),
+})
+
+//métodos dos controles do formulário
+onIncluir(){
+  this.formulario.reset();
+  this.formulario.enable();
+
+}
+
+
+onSalvar(){
+  //guarda as informações em uma variavel para melhorar o acesso
+  let info = this.formulario.value
+  //verifica se está inserido ou alterando com base no valor
+  //do ID (se for null,está inserindo,senão está alterando)
+  if(info.id == null){
+    //irá inserir no banco de dados um usuário 
+    this.usuarioService.addUsuario(info).subscribe({
+      next: (resposta)=>{
+        console.log(resposta);
+        this.snackbar.open(
+          "Usuário adicionado com sucesso!",
+          "OK",
+          {
+            verticalPosition:'top',
+            horizontalPosition:'end',
+            duration:3000
+          }
+        )
+        this.onCancelar();
+      },
+      error: (erro)=>{
+      console.log(erro)
+      this.snackbar.open(
+        "Oh não! aconteceu algo de errado",
+        "OK",
+        {
+          verticalPosition:'top',
+          horizontalPosition:'end',
+          duration:3000
+        }
+      )
+      }     
+    })
+  }else{
+    //irá alterar o usuário no banco de dados
+
+
+
+  }
+
+
+}
+
+onCancelar(){
+  this.formulario.reset();
+  this.formulario.disable();
+}
+}
